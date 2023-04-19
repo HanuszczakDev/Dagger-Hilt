a71015a7af2c1e3210b61d2703bbf513a1329c2a
ahanuszczak <hanyhusajn@gmail.com>
2023-04-19 13:15:50 +0200

    Create AppModule for dagger-hilt | provideMyApi()

---
+++ app/src/main/java/com/hanuszczak/dagger_hilt/di/AppModule.kt
@@ -0,0 +1,23 @@
+package com.hanuszczak.dagger_hilt.di
+
+import com.hanuszczak.dagger_hilt.data.remote.MyApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideMyApi(): MyApi {
+        return Retrofit.Builder()
+            .baseUrl("https://somemockurl.com")
+            .build()
+            .create(MyApi::class.java)
+    }
+}
