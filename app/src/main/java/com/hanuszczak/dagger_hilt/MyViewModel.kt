ec0d5f05386cd7579a311ab9831c936a9b62597f
ahanuszczak <hanyhusajn@gmail.com>
2023-04-19 14:16:16 +0200

    dagger.Lazy initialization

--- app/src/main/java/com/hanuszczak/dagger_hilt/MyViewModel.kt
+++ app/src/main/java/com/hanuszczak/dagger_hilt/MyViewModel.kt
@@ -5,8 +5,13 @@ import com.hanuszczak.dagger_hilt.domain.repository.MyRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+
 @HiltViewModel
 class MyViewModel @Inject constructor(
-    private val repository: MyRepository
+    private val repository: dagger.Lazy<MyRepository>
 ) : ViewModel(){
+
+    init {
+        repository.get()
+    }
 }
